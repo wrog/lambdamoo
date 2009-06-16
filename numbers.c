64f7a80aa664dbc3aeda070e0f0e7e17cf8f8786
Rob Leslie <rob@mars.org>
2009-06-16 13:15:44 -0700

    Fix ctime($maxint) panic bug
    bf_ctime() wasn't checking the return result of localtime(), causing
    strftime() to segfault whenever a null pointer was passed.

--- numbers.c
+++ numbers.c
@@ -591,6 +591,7 @@ bf_ctime(Var arglist, Byte next UNUSED_, void *vdata UNUSED_, Objid progr UNUSED
     int has_time     = (arglist.v.list[0].v.num >= 1);
     int has_timezone = (arglist.v.list[0].v.num >= 2);
     char *current_timezone = NULL;
+    struct tm *t;
 
     c = has_time ? (time_t)arglist.v.list[1].v.num : time(0);
 
@@ -602,14 +603,15 @@ bf_ctime(Var arglist, Byte next UNUSED_, void *vdata UNUSED_, Objid progr UNUSED
 	tzset();
     }
 
-    {				/* Format the time, including a timezone name */
+    t = localtime(&c);
+    if (t == 0)
+	*buffer = 0;
+    else {			/* Format the time, including a timezone name */
 #if HAVE_STRFTIME
-	if (strftime(buffer, sizeof(buffer), "%a %b %d %H:%M:%S %Y %Z",
-		     localtime(&c)) == 0)
+	if (strftime(buffer, sizeof(buffer), "%a %b %d %H:%M:%S %Y %Z", t) == 0)
 	    *buffer = 0;
 #else
 #  if HAVE_TM_ZONE
-	struct tm *t = localtime(&c);
 	char *tzname = t->tm_zone;
 #  else
 #    if !HAVE_TZNAME
