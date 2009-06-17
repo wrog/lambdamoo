ef2a8d5dffba101879bc3a43981bc66ee6f4472c
Rob Leslie <rob@mars.org>
2009-06-16 20:24:13 -0700

    Update bf_floatstr() to be 64-bit-friendly
    The MOO floatstr() built-in function raised an error if a precision greater
    than 2^31-1 was specified; we now allow it to accept any positive value.
    Note that the precision is internally limited to DECIMAL_DIG in any case.

--- numbers.c
+++ numbers.c
@@ -834,7 +834,7 @@ static package
 bf_floatstr(Var arglist, Byte next UNUSED_, void *vdata UNUSED_, Objid progr UNUSED_)
 {				/* (float, precision [, sci-notation]) */
     double d = *arglist.v.list[1].v.fnum;
-    int prec = arglist.v.list[2].v.num;
+    Num prec = arglist.v.list[2].v.num;
     int use_sci = (arglist.v.list[0].v.num >= 3
 		   && is_true(arglist.v.list[3]));
     char fmt[10], output[500];	/* enough for IEEE double */
@@ -845,7 +845,7 @@ bf_floatstr(Var arglist, Byte next UNUSED_, void *vdata UNUSED_, Objid progr UNU
 	prec = DECIMAL_DIG;
     else if (prec < 0)
 	return make_error_pack(E_INVARG);
-    sprintf(fmt, "%%.%d%c", prec, use_sci ? 'e' : 'f');
+    sprintf(fmt, "%%.%"PRIdN"%c", prec, use_sci ? 'e' : 'f');
     sprintf(output, fmt, d);
 
     r.type = TYPE_STR;
