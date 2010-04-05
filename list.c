de62ac3da49a91e392d8e2f1cef24eabd9b53529
Roger Crew <crew@cs.stanford.edu>
2010-04-05 06:22:51 -0700

    Implement max_string_concat: bf_value_hash

--- list.c
+++ list.c
@@ -987,14 +987,24 @@ bf_string_hash(Var arglist, Byte next UNUSED_, void *vdata UNUSED_, Objid progr 
 static package
 bf_value_hash(Var arglist, Byte next UNUSED_, void *vdata UNUSED_, Objid progr UNUSED_)
 {
-    Var r;
+    package p;
     Stream *s = new_stream(100);
-    unparse_value(s, arglist.v.list[1]);
-    r.type = TYPE_STR;
-    r.v.str = hash_bytes(stream_contents(s), stream_length(s));
+
+    TRY_STREAM {
+	Var r;
+
+	unparse_value(s, arglist.v.list[1]);
+	r.type = TYPE_STR;
+	r.v.str = hash_bytes(stream_contents(s), stream_length(s));
+	p = make_var_pack(r);
+    }
+    EXCEPT (stream_too_big) {
+	p = make_space_pack();
+    }
+    ENDTRY_STREAM;
     free_stream(s);
     free_var(arglist);
-    return make_var_pack(r);
+    return p;
 }
 
 static package
