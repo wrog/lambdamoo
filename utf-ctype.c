705a62e5219c1081e5d060638156fdc4af5cb983
Roger Crew <crew@cs.stanford.edu>
2023-08-21 22:57:00 -0700

    macrofy ucd interface

--- utf-ctype.c
+++ utf-ctype.c
@@ -10,6 +10,24 @@
 
 #include "storage.h"
 
+#define RETURN_UCD(_u,_ucd,_return_t,_rv_no_ucd,_rv_from_ucd)	\
+    RETURN_UCDGET(_ucd, unicode_character_data(_u),		\
+                  _return_t,_rv_no_ucd,_rv_from_ucd)
+
+#define RETURN_UCDGET(_ucd,_ucdget,				\
+		      _return_t,_rv_no_ucd,_rv_from_ucd)	\
+    const struct unicode_character_data *_ucd;			\
+								\
+    _ucd = (_ucdget);						\
+    if (!_ucd)							\
+	return (_rv_no_ucd);					\
+								\
+    _return_t rv = (_rv_from_ucd);				\
+    unicode_character_put(_ucd);				\
+								\
+    do { return rv; } while(0)
+
+
 /*------------*
  |   digits   |
  *------------*/
@@ -17,34 +35,15 @@
 int
 my_isdigit(uint32_t x)
 {
-    const struct unicode_character_data *ucd;
-    int rv;
-
-    ucd = unicode_character_data(x);
-    if (!ucd)
-	return 0;
-
-    rv = ucd->numeric_type == UC_NT_Decimal;
-    unicode_character_put(ucd);
-
-    return rv;
+    RETURN_UCD(x, ucd, int, 0, ucd->numeric_type == UC_NT_Decimal);
 }
 
 int
 my_digitval(uint32_t x)
 {
-    const struct unicode_character_data *ucd;
-    int rv;
-
-    ucd = unicode_character_data(x);
-    if (!ucd || ucd->numeric_type != UC_NT_Decimal)
-	return 0;
-
-    /* For digits, den == exp == 1 */
-    rv = ucd->numeric_value_num;
-    unicode_character_put(ucd);
-
-    return rv;
+    /* XXX assert(ucd->numeric_type == UC_NT_Decimal) */
+    /* For digits, numeric_value_{den,exp} == 1 */
+    RETURN_UCD(x, ucd, int, -1, ucd->numeric_value_num);
 }
 
 /*---------------------------*
@@ -59,33 +58,13 @@ my_digitval(uint32_t x)
 int
 my_is_xid_start(uint32_t x)
 {
-    const struct unicode_character_data *ucd;
-    int rv;
-
-    ucd = unicode_character_data(x);
-    if (!ucd)
-	return 0;
-
-    rv = !!(ucd->fl & UC_FL_XID_START);
-    unicode_character_put(ucd);
-
-    return rv;
+    RETURN_UCD(x, ucd, int, 0, !!(ucd->fl & UC_FL_XID_START));
 }
 
 int
 my_is_xid_cont(uint32_t x)
 {
-    const struct unicode_character_data *ucd;
-    int rv;
-
-    ucd = unicode_character_data(x);
-    if (!ucd)
-	return 0;
-
-    rv = !!(ucd->fl & UC_FL_XID_CONTINUE);
-    unicode_character_put(ucd);
-
-    return rv;
+    RETURN_UCD(x, ucd, int, 0, !!(ucd->fl & UC_FL_XID_CONTINUE));
 }
 
 /*-------------------------*
@@ -105,33 +84,13 @@ my_is_xid_cont(uint32_t x)
 uint32_t
 my_tolower(uint32_t x)
 {
-    const struct unicode_character_data *ucd;
-    uint32_t rv;
-
-    ucd = unicode_character_data(x);
-    if (!ucd)
-	return x;
-
-    rv = ucd->simple_lowercase;
-    unicode_character_put(ucd);
-
-    return rv;
+    RETURN_UCD(x, ucd, uint32_t, x, ucd->simple_lowercase);
 }
 
 uint32_t
 my_toupper(uint32_t x)
 {
-    const struct unicode_character_data *ucd;
-    uint32_t rv;
-
-    ucd = unicode_character_data(x);
-    if (!ucd)
-	return x;
-
-    rv = ucd->simple_uppercase;
-    unicode_character_put(ucd);
-
-    return rv;
+    RETURN_UCD(x, ucd, uint32_t, x, ucd->simple_uppercase);
 }
 
 /*----------------*
@@ -141,17 +100,7 @@ my_toupper(uint32_t x)
 int
 my_isspace(uint32_t x)
 {
-    const struct unicode_character_data *ucd;
-    int rv;
-
-    ucd = unicode_character_data(x);
-    if (!ucd)
-	return 0;
-
-    rv = !!(ucd->fl & UC_FL_WHITE_SPACE);
-    unicode_character_put(ucd);
-
-    return rv;
+    RETURN_UCD(x, ucd, int, x, !!(ucd->fl & UC_FL_WHITE_SPACE));
 }
 
 /*------------------------------*
@@ -161,23 +110,13 @@ my_isspace(uint32_t x)
 int
 my_is_printable(uint32_t x)
 {
-    const struct unicode_character_data *ucd;
-    int rv;
-
     if (x == 0x09)
         return 1;
     if ((x <= 0xff && ((x & 0x60) == 0x00 || x == 0x7f)) ||
 	(x >= 0xd800 && x <= 0xdfff))
         return 0;
 
-    ucd = unicode_character_data(x);
-    if (!ucd)
-        return 0;
-
-    rv = !(ucd->fl & UC_FL_NONCHARACTER_CODE_POINT);
-    unicode_character_put(ucd);
-
-    return rv;
+    RETURN_UCD(x, ucd, int, 0, !(ucd->fl & UC_FL_NONCHARACTER_CODE_POINT));
 }
 
 /*----------------------------------*
@@ -187,31 +126,12 @@ my_is_printable(uint32_t x)
 const char *
 my_char_name(uint32_t x)
 {
-    const struct unicode_character_data *ucd;
-    char *rv;
-
-    ucd = unicode_character_data(x);
-    if (!ucd)
-	return (NULL);
-
-    rv = str_dup(ucd->name);
-    unicode_character_put(ucd);
-
-    return rv;
+    RETURN_UCD(x, ucd, char *, NULL, str_dup(ucd->name));
 }
 
 uint32_t
 my_char_lookup(const char *name)
 {
-    const struct unicode_character_data *ucd;
-    uint32_t rv;
-
-    ucd = unicode_character_lookup(name);
-    if (!ucd)
-	return (0);
-
-    rv = ucd->ucs;
-    unicode_character_put(ucd);
-
-    return rv;
+    RETURN_UCDGET(ucd, unicode_character_lookup(name),
+		  uint32_t, 0, ucd->ucs);
 }
