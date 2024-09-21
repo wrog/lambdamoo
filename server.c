6f947dbf088ddf913e4f50a7a550e1d7ee7ad646
Roger Crew <crew@cs.stanford.edu>
2024-09-20 23:29:31 -0700

    add cppdef NOMAIN to server.c for unit testing

--- server.c
+++ server.c
@@ -1181,6 +1181,7 @@ read_active_connections(void)
     return 1;
 }
 
+#ifndef NOMAIN
 int
 main(int argc, char **argv)
 {
@@ -1277,7 +1278,7 @@ main(int argc, char **argv)
 
     return 0;
 }
-
+#endif
 
 /**** built in functions ****/
 
