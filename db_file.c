scm-repo-mirror/wrog_lambdamoo

dbio_scanf -> dbio_scxnf
scanf has too many annoying behaviors so we are replacing it.

E.g., Pavel was unhappy about %* specs not being counted in the
return value and was having to add 'dummy' variables all over.

I myself am unhappy about how you can put a \n at the end of a pattern
giving the false sense that you want it to match a newline when in
fact there's no guarantee of that at all, or that, in ANY pattern with
characters after the last % spec that you'll have any idea whether
those characters were reached or not (hence the need for all of these
%c-at-the-end bullshit patterns)

But worst of all, you cannot count on E_RANGE errors being raised when
they're needed because there's disagreement about when that should
happen, putting this in the realm of undefined behavior (hence certain
vendors putting deprecation warnings on %d and all of the other integer
conversions)

So.., fuck it.  We write our own function from the ground up that does
what we need (and very little more) and will thence be completely
portable (knock wood).

Well okay, it still relies on strtoimax() continuing to behave sensibly.
We'll see how that goes.

  --wrog

db_file.c
@@ -46,7 +46,7 @@
 static char *input_db_name, *dump_db_name;
 static int dump_generation = 0;
 static const char *header_format_string
-= "** LambdaMOO Database, Format Version %u **\n";
+= "** LambdaMOO Database, Format Version %u **";
 
 DB_Version dbio_input_version;
 
@@ -113,17 +113,21 @@ read_object(void)
     Objid oid;
     Object *o;
 
-    if (dbio_scanf("#%"SCNdN, &oid) != 1 || oid != db_last_used_objid() + 1)
+    int sc = dbio_scxnf("#%"SCNdN"\v recycled", &oid);
+    if (!sc) {
+	errlog("READ_OBJECT: Bad first line\n");
 	return 0;
+    }
+    if (oid != db_last_used_objid() + 1) {
+	errlog("READ_OBJECT: Object number is out of order (expecting 1+#%"PRIdN")\n",
+	       db_last_used_objid());
+	return 0;
+    }
 
-    const char *s;
-    if (!dbio_read_string_temp(&s))
-	return 0;
-    else if (strcmp(s, " recycled") == 0) {
+    if (sc == 2) {
 	dbpriv_new_recycled_object();
 	return 1;
-    } else if (s[0])
-	return 0;
+    }
 
     /* Every 'return 0' from here to the end of this function
      * should arguably be jumping to or calling a routine that
@@ -422,30 +426,36 @@ static int
 read_db_file(void)
 {
     Objid oid;
-    Num i, nobjs, nprogs, nusers, vnum, dummy;
+    UNum i, nobjs, nprogs, nusers, vnum;
     Var user_list;
     db_verb_handle h;
     Program *program;
 
-    if (dbio_scanf(header_format_string, &dbio_input_version) != 1)
+    /* Evidently, prehistory DBs had no header line, they would just
+     * go straight to the object count.  Therefore, a prehistory DB
+     * will not start with '*'.  Since stdio allows us to put back
+     * one character, we can do a quick probe.  I prefer this to
+     * further messing with dbio_scxnf.  --wrog
+     */
+    if (header_format_string[0] != dbio_peek_byte()) {
 	dbio_input_version = DBV_Prehistory;
-
-    if (!check_db_version(dbio_input_version)) {
-	errlog("READ_DB_FILE: Unknown DB version number: %d\n",
+    }
+    else if (!dbio_scxnf(header_format_string, &dbio_input_version)) {
+	errlog("READ_DB_FILE: Bad DB header (no version?)\n");
+	return 0;
+    }
+    else if (!check_db_version(dbio_input_version)) {
+	errlog("READ_DB_FILE: Unknown DB version number: %u\n",
 	       dbio_input_version);
 	return 0;
     }
-    /* I use a `dummy' variable here and elsewhere instead of the `*'
-     * assignment-suppression syntax of `scanf' because it allows more
-     * straightforward error checking; unfortunately, the standard says that
-     * suppressed assignments are not counted in determining the returned value
-     * of `scanf'...
-     */
-    if (dbio_scanf("%"SCNdN"\n%"SCNdN"\n%"SCNdN"\n%"SCNdN"\n",
-		   &nobjs, &nprogs, &dummy, &nusers) != 4) {
-	errlog("READ_DB_FILE: Bad header\n");
+
+    if (!dbio_scxnf("%"SCNuN"\n%"SCNuN"\n%*d\n%"SCNuN,
+		    &nobjs, &nprogs, &nusers)) {
+	errlog("READ_DB_FILE: Bad DB header (missing counts?)\n");
 	return 0;
     }
+
     user_list = new_list(nusers);
     for (i = 1; i <= nusers; i++) {
 	user_list.v.list[i].type = TYPE_OBJ;
@@ -470,7 +480,7 @@ read_db_file(void)
     }
     oklog("LOADING: Reading %"PRIdN" MOO verb programs...\n", nprogs);
     for (i = 1; i <= nprogs; i++) {
-	if (dbio_scanf("#%"SCNdN":%"SCNdN"\n", &oid, &vnum) != 2) {
+	if (!dbio_scxnf("#%"SCNdN":%"SCNdN, &oid, &vnum)) {
 	    errlog("READ_DB_FILE: Bad program header, i = %"PRIdN".\n", i);
 	    return 0;
 	}
@@ -533,7 +543,7 @@ write_db_file(const char *reason)
 
     TRY {
 	dbio_printf(header_format_string, current_db_version);
-	dbio_printf("%"PRIdN"\n%d\n%d\n%"PRIdN"\n",
+	dbio_printf("\n%"PRIdN"\n%d\n%d\n%"PRIdN"\n",
 		    max_oid + 1, nprogs, 0, user_list.v.list[0].v.num);
 	for (i = 1; i <= user_list.v.list[0].v.num; i++)
 	    dbio_write_objid(user_list.v.list[i].v.obj);

db_io.c
@@ -133,19 +133,6 @@ dbio_read_line_noisy(const char *caller, const char **s, const char **pend)
     return 0;
 }
 
-int
-dbio_scanf(const char *format,...)
-{
-    va_list args;
-    int count;
-
-    va_start(args, format);
-    count = vfscanf(input, format, args);
-    va_end(args);
-
-    return count;
-}
-
 /*--------------------------*
  |  integer range checking  |
  *--------------------------*/
@@ -153,6 +140,7 @@ dbio_scanf(const char *format,...)
 #define  DBIO_INT16_INIT  { INT16_MIN,  INT16_MAX, 0 }
 #define DBIO_UINT16_INIT  {         0, UINT16_MAX, 0 }
 #define DBIO_INTMAX_INIT  { .skip=1 }
+#define   DBIO_TASK_INIT  {  TASK_MIN,   TASK_MAX, 0 }
 #define    DBIO_ERR_INIT  {   ERR_MIN,    ERR_MAX, 0 }
 
 #if NUM_MAX == INTMAX_MAX
@@ -192,7 +180,6 @@ static struct intrange {
        DBIO_INT_TYPE_LIST(DBIO_DO_)
 };
 #undef DBIO_DO_
-
 
 static intmax_t
 dbio_string_to_integer(enum dbio_intrange range_id, const char *s, const char **end)
@@ -350,6 +337,196 @@ dbio_read_var_value(intmax_t vtype, Var *vp)
 	return 0;
     }
     return 1;
+}
+
+/*---------------------*
+ |  dbio_scxnf         |
+ *---------------------*/
+
+static struct scanfmt {
+    enum dbio_intrange range_id;
+    const char *cspec;
+    size_t cslen;
+}
+#define DBIO_DO_(INTXX,_2,_3,SCNxdd)				\
+        { DBIO_RANGE_##INTXX, SCNxdd, (sizeof SCNxdd)-1 },	\
+
+    dbio_scanfmts[] = {
+        DBIO_INT_TYPE_LIST(DBIO_DO_)
+	{ DBIO_RANGE__LENGTH, NULL, 0 }
+};
+#undef DBIO_DO_
+
+int
+dbio_scxnf(const char *format,...)
+{
+    va_list args;
+    va_start(args, format);
+
+/* Famous Last Words (Pavel):
+ * "... Fortunately, we only use a small fraction of the full
+ *      functionality of scanf in the server, so it's not
+ *      unbearably unpleasant to have to reimplement it here."
+ */
+
+    int rcount = 1;
+    const char *line, *lend, *lc;
+    const char *fc = format;
+    dbio_last_error = NULL;
+
+ nextline:
+    lc = line = dbio_read_line(&lend);
+    if (!line) {
+	/* format must be entirely optional lines
+	 * from here on; skip all of them.
+	 */
+	while (*fc == '\v') {
+	    do if (!*++fc) goto done;
+	    while (*fc != '\n');
+	    ++fc;
+	}
+	dbio_last_error = "premature EOF";
+	goto fail;
+    }
+    if (*fc == '\v') { ++fc; ++rcount; }
+
+ state1:
+    /* lc is somewhere along an actual subject line.
+     * fc is somewhere along a line format *after* any initial \v,
+     * meaning any \v we encounter now is a middle-of-the-line '\v'.
+     * Every prior \v for this line has been resolved
+     * (i e. chars were present, therefore this segment is
+     * no longer optional).
+     */
+    if (lc == lend) {
+	/* the 6 cases for *fc we have to deal with:
+	 *  <space>  \0  \v  %  everything-else  \n
+	 */
+	while (*fc == ' ') ++fc;
+	if (!*fc) goto done;
+	if (*fc == '\v') {
+	    do if (!*++fc) goto done;
+	    while (*fc != '\n');
+	}
+	else if (*fc == '%')
+	    goto convspec;
+	else if (*fc != '\n') {
+	    dbio_last_error = "could not match entire format";
+	    goto fail;
+	}
+	++fc;
+	goto nextline;
+    }
+    /* the 6 cases for *fc, in a different order:
+     *  \v  \0  \n  <space>  everything-else  %
+     */
+    while (*fc == '\v') { ++fc;	++rcount; }
+    if (!*fc || *fc == '\n') {
+	dbio_last_error = "unexpected junk at end-of-line";
+	goto fail;
+    }
+    if (*fc == ' ') {
+	while (isspace(*lc) && ++lc < lend);
+	++fc;
+	goto state1;
+    }
+    if (*fc != '%') {
+	if (!(lc < lend && *lc == *fc)) {
+	    dbio_last_error = "character mismatch";
+	    goto fail;
+	}
+	++lc;
+	++fc;
+	goto state1;
+    }
+    /*
+     * We have a conversion spec (*fc == '%')
+     * (note end-of-line case jumps here because
+     * eol makes no difference for '%'s,
+     * so from here down, *lc could be '\0
+     * (or equivalently we could have lc == lcend)
+     */
+ convspec: ;
+    /* recall the five classes of conversion spec
+     * we have to handle:
+     *
+     *    %ms   %*s   %*d   %c   %<...>(du)
+     */
+    int noassign = 0;
+    if (*++fc == '*') {
+	++fc;
+	noassign = 1;
+    }
+    if (*fc == 'm') {		/* %ms */
+	if (noassign)
+	    panic("DBIO_SCXNF: %*m... makes no sense");
+	if (*++fc != 's')
+	    panic("DBIO_SCXNF: %m must be followed by 's'");
+	if (*++fc)
+	    panic("DBIO_SCXNF: %ms must be at the end");
+	const char **sp = va_arg(args, const char **);
+	(*sp) = lc;
+	goto done;
+    }
+    if (*fc == 's') {		/* %*s */
+	if (!noassign)
+	    panic("DBIO_SCXNF: missing 'm' (must be %ms, not %s)");
+	if (!*++fc)
+	    goto done;
+	if (*fc++ != '\n')
+	    panic("DBIO_SCXNF: %*s can only be followed by a newline");
+	goto nextline;
+    }
+    if (noassign) {		/* %*d */
+	if (*fc++ != 'd')
+	    panic("DBIO_SCXNF: %* can only be followed by s or d");
+	if (*lc == '-') ++lc;
+	if (!isdigit(*lc)) {
+	    dbio_last_error = "expected an integer to skip";
+	    goto fail;
+	}
+	while (isdigit(*++lc));
+	goto state1;
+    }
+    if (*fc == 'c') {		/* %c */
+	++fc;
+	char *cp = va_arg(args, char *);
+	*cp = *lc++;
+	goto state1;
+    }
+    /* We have to actually read a number and assign it */
+    struct scanfmt *sf = dbio_scanfmts;
+    while ((sf->cspec
+	    || (panic("DBIO_SCXNF: Unsupported directive!"),0))
+	   && (0 != strncmp(sf->cspec, fc, sf->cslen)))
+	++sf;
+    fc += sf->cslen;
+
+    intmax_t i = dbio_string_to_integer(sf->range_id, lc, &lc);
+    if (dbio_last_error)
+	goto fail;
+
+#define DBIO_DO_(INTXX,intxx_t,_3,_4)			\
+	case DBIO_RANGE_##INTXX: {			\
+	    intxx_t *ip = va_arg(args, intxx_t *);	\
+	    *ip = (intxx_t)i;				\
+	    break;					\
+	}						\
+
+    switch (sf->range_id) {
+	DBIO_INT_TYPE_LIST(DBIO_DO_);
+    default:
+	/* really should not happen */
+	panic("DBIO_SCXNF: Unsupported directive?");
+    }
+#undef DBIO_DO_
+    goto state1;
+
+ fail:
+    rcount = 0;
+ done:
+    va_end(args);
+    return rcount;
 }
 
 /*---------------------*

wrog/lambdamoo

db_file.c
@@ -44,7 +44,7 @@
 static char *input_db_name, *dump_db_name;
 static int dump_generation = 0;
 static const char *header_format_string
-= "** LambdaMOO Database, Format Version %u **\n";
+= "** LambdaMOO Database, Format Version %u **";
 
 DB_Version dbio_input_version;
 
@@ -111,17 +111,21 @@ read_object(void)
     Objid oid;
     Object *o;
 
-    if (dbio_scanf("#%"SCNdN, &oid) != 1 || oid != db_last_used_objid() + 1)
+    int sc = dbio_scxnf("#%"SCNdN"\v recycled", &oid);
+    if (!sc) {
+	errlog("READ_OBJECT: Bad first line\n");
 	return 0;
+    }
+    if (oid != db_last_used_objid() + 1) {
+	errlog("READ_OBJECT: Object number is out of order (expecting 1+#%"PRIdN")\n",
+	       db_last_used_objid());
+	return 0;
+    }
 
-    const char *s;
-    if (!dbio_read_string(&s))
-	return 0;
-    else if (strcmp(s, " recycled") == 0) {
+    if (sc == 2) {
 	dbpriv_new_recycled_object();
 	return 1;
-    } else if (s[0])
-	return 0;
+    }
 
     /* Every 'return 0' from here to the end of this function
      * should arguably be jumping to or calling a routine that
@@ -420,30 +424,36 @@ static int
 read_db_file(void)
 {
     Objid oid;
-    Num i, nobjs, nprogs, nusers, vnum, dummy;
+    Num i, nobjs, nprogs, nusers, vnum;
     Var user_list;
     db_verb_handle h;
     Program *program;
 
-    if (dbio_scanf(header_format_string, &dbio_input_version) != 1)
+    /* Evidently, prehistory DBs had no header line, they would just
+     * go straight to the object count.  Therefore, a prehistory DB
+     * will not start with '*'.  Since stdio allows us to put back
+     * one character, we can do a quick probe.  I prefer this to
+     * further messing with dbio_scxnf.  --wrog
+     */
+    if (header_format_string[0] != dbio_peek_byte()) {
 	dbio_input_version = DBV_Prehistory;
-
-    if (!check_db_version(dbio_input_version)) {
-	errlog("READ_DB_FILE: Unknown DB version number: %d\n",
+    }
+    else if (!dbio_scxnf(header_format_string, &dbio_input_version)) {
+	errlog("READ_DB_FILE: Bad DB header (no version?)\n");
+	return 0;
+    }
+    else if (!check_db_version(dbio_input_version)) {
+	errlog("READ_DB_FILE: Unknown DB version number: %u\n",
 	       dbio_input_version);
 	return 0;
     }
-    /* I use a `dummy' variable here and elsewhere instead of the `*'
-     * assignment-suppression syntax of `scanf' because it allows more
-     * straightforward error checking; unfortunately, the standard says that
-     * suppressed assignments are not counted in determining the returned value
-     * of `scanf'...
-     */
-    if (dbio_scanf("%"SCNdN"\n%"SCNdN"\n%"SCNdN"\n%"SCNdN"\n",
-		   &nobjs, &nprogs, &dummy, &nusers) != 4) {
-	errlog("READ_DB_FILE: Bad header\n");
+
+    if (!dbio_scxnf("%"SCNdN"\n%"SCNdN"\n%*d\n%"SCNdN,
+		    &nobjs, &nprogs, &nusers)) {
+	errlog("READ_DB_FILE: Bad DB header (missing counts?)\n");
 	return 0;
     }
+
     user_list = new_list(nusers);
     for (i = 1; i <= nusers; i++) {
 	user_list.v.list[i].type = TYPE_OBJ;
@@ -468,7 +478,7 @@ read_db_file(void)
     }
     oklog("LOADING: Reading %"PRIdN" MOO verb programs...\n", nprogs);
     for (i = 1; i <= nprogs; i++) {
-	if (dbio_scanf("#%"SCNdN":%"SCNdN"\n", &oid, &vnum) != 2) {
+	if (!dbio_scxnf("#%"SCNdN":%"SCNdN, &oid, &vnum)) {
 	    errlog("READ_DB_FILE: Bad program header, i = %"PRIdN".\n", i);
 	    return 0;
 	}
@@ -531,7 +541,7 @@ write_db_file(const char *reason)
 
     TRY {
 	dbio_printf(header_format_string, current_db_version);
-	dbio_printf("%"PRIdN"\n%d\n%d\n%"PRIdN"\n",
+	dbio_printf("\n%"PRIdN"\n%d\n%d\n%"PRIdN"\n",
 		    max_oid + 1, nprogs, 0, user_list.v.list[0].v.num);
 	for (i = 1; i <= user_list.v.list[0].v.num; i++)
 	    dbio_write_objid(user_list.v.list[i].v.obj);

db_io.c
@@ -139,19 +139,6 @@ dbio_read_line_noisy(const char *caller, const char **s, const char **pend)
     return 0;
 }
 
-int
-dbio_scanf(const char *format,...)
-{
-    va_list args;
-    int count;
-
-    va_start(args, format);
-    count = vfscanf(input, format, args);
-    va_end(args);
-
-    return count;
-}
-
 /*--------------------------*
  |  integer range checking  |
  *--------------------------*/
@@ -195,7 +182,6 @@ static struct intrange {
        DBIO_RANGE_SPEC_LIST(DBIO_DO_)
 };
 #undef DBIO_DO_
-
 
 static intmax_t
 dbio_string_to_integer(enum dbio_intrange range_id, const char *s, const char **end)
@@ -346,6 +332,190 @@ dbio_read_var_value(intmax_t vtype, Var *vp)
 	return 0;
     }
     return 1;
+}
+
+/*---------------------*
+ |  dbio_scxnf         |
+ *---------------------*/
+
+static struct scanfmt {
+    enum dbio_intrange range_id;
+    const char *cspec;
+    size_t cslen;
+}
+#define DBIO_DO_(INTXX,_2,_3,SCNxdd)				\
+        { DBIO_RANGE_##INTXX, SCNxdd, (sizeof SCNxdd)-1 },	\
+
+    dbio_scanfmts[] = {
+        DBIO_RANGE_SPEC_LIST(DBIO_DO_)
+	{ DBIO_RANGE__LENGTH, NULL, 0 }
+};
+#undef DBIO_DO_
+
+int
+dbio_scxnf(const char *format,...)
+{
+    va_list args;
+    va_start(args, format);
+
+/* Famous Last Words (Pavel):
+ * "... Fortunately, we only use a small fraction of the full
+ *      functionality of scanf in the server, so it's not
+ *      unbearably unpleasant to have to reimplement it here."
+ */
+
+    int rcount = 1;
+    const char *line, *lend, *lc;
+    const char *fc = format;
+    dbio_last_error = NULL;
+
+ nextline:
+    lc = line = dbio_read_line(&lend);
+    if (!line) {
+	/* format must be entirely optional lines
+	 * from here on; skip all of them.
+	 */
+	while (*fc == '\v') {
+	    do if (!*++fc) goto done;
+	    while (*fc != '\n');
+	    ++fc;
+	}
+	dbio_last_error = "premature EOF";
+	goto fail;
+    }
+    if (*fc == '\v') { ++fc; ++rcount; }
+
+ state1:
+    /* lc is somewhere along an actual subject line.
+     * fc is somewhere along a line format *after* any initial \v,
+     * meaning any \v we encounter now is a middle-of-the-line '\v'.
+     * Every prior \v for this line has been resolved
+     * (i e. chars were present, therefore this segment is
+     * no longer optional).
+     */
+    if (lc == lend) {
+	/* the 6 cases for *fc we have to deal with:
+	 *  <space>  \0  \v  %  everything-else  \n
+	 */
+	while (*fc == ' ') ++fc;
+	if (!*fc) goto done;
+	if (*fc == '\v') {
+	    do if (!*++fc) goto done;
+	    while (*fc != '\n');
+	}
+	else if (*fc == '%')
+	    goto convspec;
+	else if (*fc != '\n') {
+	    dbio_last_error = "could not match entire format";
+	    goto fail;
+	}
+	++fc;
+	goto nextline;
+    }
+    /* the 6 cases for *fc, in a different order:
+     *  \v  \0  \n  <space>  everything-else  %
+     */
+    while (*fc == '\v') { ++fc;	++rcount; }
+    if (!*fc || *fc == '\n') {
+	dbio_last_error = "unexpected junk at end-of-line";
+	goto fail;
+    }
+    if (*fc == ' ') {
+	while (isspace(*lc) && ++lc < lend);
+	++fc;
+	goto state1;
+    }
+    if (*fc != '%') {
+	if (!(lc < lend && *lc == *fc)) {
+	    dbio_last_error = "character mismatch";
+	    goto fail;
+	}
+	++lc;
+	++fc;
+	goto state1;
+    }
+    /*
+     * We have a conversion spec (*fc == '%')
+     * (note end-of-line case jumps here because
+     * eol makes no difference for '%'s,
+     * so from here down, *lc could be '\0
+     * (or equivalently we could have lc == lcend)
+     */
+ convspec: ;
+    /* recall the four classes of conversion spec
+     * we have to handle:
+     *
+     *    %ms   %*s   %*d   %<...>(du)
+     */
+    int noassign = 0;
+    if (*++fc == '*') {
+	++fc;
+	noassign = 1;
+    }
+    if (*fc == 'm') {		/* %ms */
+	if (noassign)
+	    panic("DBIO_SCXNF: %*m... makes no sense");
+	if (*++fc != 's')
+	    panic("DBIO_SCXNF: %m must be followed by 's'");
+	if (*++fc)
+	    panic("DBIO_SCXNF: %ms must be at the end");
+	const char **sp = va_arg(args, const char **);
+	(*sp) = lc;
+	goto done;
+    }
+    if (*fc == 's') {		/* %*s */
+	if (!noassign)
+	    panic("DBIO_SCXNF: missing 'm' (must be %ms, not %s)");
+	if (!*++fc)
+	    goto done;
+	if (*fc++ != '\n')
+	    panic("DBIO_SCXNF: %*s can only be followed by a newline");
+	goto nextline;
+    }
+    if (noassign) {		/* %*d */
+	if (*fc++ != 'd')
+	    panic("DBIO_SCXNF: %* can only be followed by s or d");
+	if (*lc == '-') ++lc;
+	if (!isdigit(*lc)) {
+	    dbio_last_error = "expected an integer to skip";
+	    goto fail;
+	}
+	while (isdigit(*++lc));
+	goto state1;
+    }
+    /* We have to actually read a number and assign it */
+    struct scanfmt *sf = dbio_scanfmts;
+    while ((sf->cspec
+	    || (panic("DBIO_SCXNF: Unsupported directive!"),0))
+	   && (0 != strncmp(sf->cspec, fc, sf->cslen)))
+	++sf;
+    fc += sf->cslen;
+
+    intmax_t i = dbio_string_to_integer(sf->range_id, lc, &lc);
+    if (dbio_last_error)
+	goto fail;
+
+#define DBIO_DO_(INTXX,intxx_t,_3,_4)			\
+	case DBIO_RANGE_##INTXX: {			\
+	    intxx_t *ip = va_arg(args, intxx_t *);	\
+	    *ip = (intxx_t)i;				\
+	    break;					\
+	}						\
+
+    switch (sf->range_id) {
+	DBIO_RANGE_SPEC_LIST(DBIO_DO_);
+    default:
+	/* really should not happen */
+	panic("DBIO_SCXNF: Unsupported directive?");
+    }
+#undef DBIO_DO_
+    goto state1;
+
+ fail:
+    rcount = 0;
+ done:
+    va_end(args);
+    return rcount;
 }
 
 /*---------------------*